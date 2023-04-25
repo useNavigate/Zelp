a9baff1451fb91fe455eeec1296570c8cac60af0
useNavigate <useNavigate@gmail.com>
2023-04-25 17:13:37 -0400

    fixed keybordshortcut popup for map

--- frontend/src/components/Map/Marker.js
+++ frontend/src/components/Map/Marker.js
@@ -4,37 +4,38 @@ import { useDispatch, useSelector } from "react-redux";
 import { useState, useMemo, useEffect } from "react";
 import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
 
-
-
 const place = ["places"];
 export default function BizGMap({ business }) {
+
   const { isLoaded } = useLoadScript({
     googleMapsApiKey: process.env.REACT_APP_MAPS_API_KEY,
     libraries: place,
     // libraries: ["places"]
   });
-  if(!business){
-    return null
+  if (!business) {
+    return null;
   }
   if (!isLoaded) return <div>Loading...</div>;
   return <Map business={business} />;
 }
 
 function Map({ business }) {
-  const center = useMemo(() => ({ lat:business.lat, lng: business.lng }), [business]);
+  const center = useMemo(
+    () => ({ lat: business.lat, lng: business.lng }),
+    [business]
+  );
   const [selected, setSelected] = useState(null);
-
-
 
   return (
     <>
-      <div className="places-container">
-
-      </div>
+      <div className="places-container"></div>
       <GoogleMap
         zoom={15}
         center={center}
         mapContainerClassName="map-container"
+        options={{
+          keyboardShortcuts: false, // Disable keyboard shortcuts
+        }}
       >
         <Marker
           position={{
@@ -43,6 +44,8 @@ function Map({ business }) {
           }}
           animation={1}
         />
+
+
       </GoogleMap>
     </>
   );
