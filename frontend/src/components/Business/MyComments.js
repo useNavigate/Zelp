44bbd392a0b53ce49bc3dcd32be4902204eebd28
useNavigate <useNavigate@gmail.com>
2023-05-02 16:57:50 -0400

    yourReview

--- frontend/src/components/Business/MyComments.js
+++ frontend/src/components/Business/MyComments.js
@@ -7,58 +7,59 @@ const MyComments = ({ review, business, bizId, myReview }) => {
   const { id } = useParams();
 
   return (
-    <div className="_comments">
-      <div className="authorProfile myReview">
-        <h1 className="yourReview"> Your Review</h1>
-
-        <div className="myInfoComment">
-          <div className="userInfoSide">
-            {review?.avatarUrl ? (
-              <img
-                className="userProfilePicURL"
-                src={`${review.avatarUrl}`}
-                width="60px"
-                height="60px"
+    <>
+      <div className="allComments">Your Review</div>
+      <div className="_comments">
+        <div className="authorProfile myReview">
+          <div className="myInfoComment">
+            <div className="userInfoSide">
+              {review?.avatarUrl ? (
+                <img
+                  className="userProfilePicURL"
+                  src={`${review.avatarUrl}`}
+                  width="60px"
+                  height="60px"
+                />
+              ) : (
+                <i className="fa-solid fa-user" />
+              )}
+              <h1>
+                {review.firstName} {review.lastName[0]}
+              </h1>
+            </div>
+            <div>
+              <CommentDropDown
+                reviewId={review.id}
+                business={business}
+                bizId={bizId}
+                myReview={myReview}
               />
-            ) : (
-              <i className="fa-solid fa-user" />
-            )}
-            <h1>
-              {review.firstName} {review.lastName[0]}
-            </h1>
+            </div>
           </div>
-          <div>
-            <CommentDropDown
-              reviewId={review.id}
-              business={business}
-              bizId={bizId}
-              myReview={myReview}
-            />
+          <div className="commentBody">
+            <div className="ratingDate">
+              <FixedStarRating rating={review?.rating} />
+              <h1>{review?.createdAt?.split("T")[0].split("-").join("/")}</h1>
+            </div>
           </div>
-        </div>
-        <div className="commentBody">
-          <div className="ratingDate">
-            <FixedStarRating rating={review?.rating} />
-            <h1>{review?.createdAt?.split("T")[0].split("-").join("/")}</h1>
+          <p className="commentReviewBodyP">{review.body}</p>
+          <div className="ReviewPicDiv">
+            {review?.imageUrls?.map((url, i) => {
+              return (
+                <div
+                  className="userReviewPictures"
+                  style={{
+                    backgroundImage: `url(${url})`,
+                    backgroundSize: "cover",
+                  }}
+                  key={url + i}
+                ></div>
+              );
+            })}
           </div>
-        </div>
-        <p className="commentReviewBodyP">{review.body}</p>
-        <div className="ReviewPicDiv">
-          {review?.imageUrls?.map((url, i) => {
-            return (
-              <div
-                className="userReviewPictures"
-                style={{
-                  backgroundImage: `url(${url})`,
-                  backgroundSize: "cover",
-                }}
-                key={url + i}
-              ></div>
-            );
-          })}
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
