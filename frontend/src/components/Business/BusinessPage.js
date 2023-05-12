53d8af651f257842489db16b1807340359b49893
useNavigate <useNavigate@gmail.com>
2023-05-11 22:52:36 -0400

    project ad comp

--- frontend/src/components/Business/BusinessPage.js
+++ frontend/src/components/Business/BusinessPage.js
@@ -11,6 +11,8 @@ import MyComments from "./MyComments";
 import EditReviewButton from "./EditReviewButton.js";
 import { Link } from "react-router-dom";
 import Loading from "../Utils/Loading";
+import ContactMe from "./ContactMe";
+import ProjectCard from "./ProjectCard";
 
 const BusinessPage = () => {
   const dispatch = useDispatch();
@@ -76,36 +78,8 @@ const BusinessPage = () => {
             })}
         </div>
         <div className="sideBar">
-          <div className="getDirection">
-            <h1 className="contactMe">Contact Me</h1>
-
-            <div className="sara">
-              <h2>Full Stack Developer & Creative Designer</h2>
-              <p>Bridging the Gap Between Functionality & Aesthetics</p>
-            </div>
-            <div className="sara_links">
-              <a
-                href="https://www.linkedin.com/in/sara-ryu-798165261/"
-                target="_blank"
-              >
-                <h1>LinkedIn</h1>
-              </a>
-              <a href="https://github.com/useNavigate" target="_blank">
-                <h1>GitHub</h1>
-              </a>
-              <a href="https://wellfound.com/u/sara-ryu" target="_blank">
-                <h1>Wellfound</h1>
-              </a>
-            </div>
-
-              <a
-                className="resumeLink"
-                href="https://cdn.discordapp.com/attachments/1082730279044657222/1105628541313355856/resume_-_Google_Docs.pdf"
-              >
-                Resume
-              </a>
-
-          </div>
+          <ContactMe />
+          {/* <ProjectCard url="https://cdn.discordapp.com/attachments/1082730279044657222/1105607720310157352/whale.png"/> */}
         </div>
       </div>
     </div>
---
+++ frontend/src/components/Business/ProjectCard.js
@@ -0,0 +1,17 @@
+const ProjectCard = ({url}) => {
+  return (
+    <div
+      className="projectCard"
+      style={{
+        backgroundImage: `url(${url})`,
+        backgroundSize: "fit",
+        backgroundPosition: "center",
+      }}
+    >
+      <h1>Whale Gaming</h1>
+      <h2>Top gaming Information</h2>
+    </div>
+  );
+};
+
+export default ProjectCard;
