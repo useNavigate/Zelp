3cff17c910e6868adb9b45d2c7bd9926ab57352e
useNavigate <useNavigate@gmail.com>
2023-05-22 13:50:12 -0400

    updated resume file

--- frontend/src/components/Business/ContactMe.js
+++ frontend/src/components/Business/ContactMe.js
@@ -24,7 +24,7 @@ const ContactMe=()=>{
 
         <a
           className="resumeLink"
-          href="https://cdn.discordapp.com/attachments/1082730279044657222/1110257443390378065/sararyu_res.pdf"
+          href="https://cdn.discordapp.com/attachments/1082730279044657222/1110257443390378065/sararyu_res.pdf" target="_blank"
         >
           Resume
         </a>
